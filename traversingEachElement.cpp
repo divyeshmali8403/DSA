b744a0ec8c68b5b4d0023ecc2154c95c704281f5
divyeshmali8403 <105155316+divyeshmali8403@users.noreply.github.com>
2022-05-08 17:34:23 +0530

    Traversing Each Element in DSA

---
+++ traversingEachElement.cpp
@@ -0,0 +1,29 @@
+
+// // Traversing : Visit each and every element of an array.
+
+// program : - >
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+    
+//     int len;
+//     cout<<"Enter length of an Array : ";
+//     cin>>len;
+    
+//     int arr[len];
+    
+//     for(int i = 0; i < len; i++) {
+//         cout<<"Enter "<<i+1<<" Element : ";
+//         cin>>arr[i];
+//     }
+    
+//     for(int i = 0; i < len; i++) {
+//       cout<<i+1<<" : "<<arr[i]<<endl;  
+//     }
+    
+//     return 0;
+// }
+
